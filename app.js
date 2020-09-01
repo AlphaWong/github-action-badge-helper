31db4b77ae34144fd16d56561e587ef364ce06bb
Alpha <AlphaWong@users.noreply.github.com>
2020-09-01 16:36:14 +0800

    feat: reset button

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 (() => {
-  const generate = async (event) => {
+  const generate = () => {
     const repo = document.getElementById("repo").value;
     const workflow = document.getElementById("workflow").value;
     if (!repo.split("https://github.com/")[1] || workflow.length === 0) {
       return;
     }
-    // https://github.com/goceevo/quality-assurance
-    // E2E in custom container
-    // https://github.com/goceevo/quality-assurance/workflows/E2E%20in%20custom%20container/badge.svg?branch=develop
     const [owner, repository] = repo.split("https://github.com/")[1].split("/");
     const preview = `https://github.com/${owner}/${repository}/workflows/${encodeURIComponent(
       workflow
     )}/badge.svg?branch=master`;
     document.getElementById("code").textContent = preview;
-    console.log(preview);
   };
   document.getElementById("generate").onclick = generate;
+
+  const reset = () => {
+    document.getElementById("my-form").reset();
+    document.getElementById("code").textContent = "";
+  };
+  document.getElementById("clear").onclick = reset;
 })();
